4eb5c7dd158c3589412ce4546499b73442c4daef
Louis Savoie <thelousavoie@gmail.com>
2021-04-20 19:40:11 -0400

    fix: list karma command not reporting error

--- commands/listKarma.js
+++ commands/listKarma.js
@@ -8,8 +8,16 @@ module.exports = {
         // check if the database has things with names containing with the character
         let regex = new RegExp(char,"i");
         Thing.find({name: regex}, function(err, foundThings) {
+            // if it doesn't, send DM to message's author with error
+            if (foundThings.length == 0){
+                message.reply({
+                    embed: {
+                      color: "RED",
+                      description: `No things starting with **${char}** exist!`
+                    }
+                }).catch(console.error);
             // if it does, send DM to the message's author with thing's karma
-            if (foundThings){
+            } else {
                 console.log("foundThings:\n" + foundThings);
                 text = `.\nThings containing **${char}**:`;
                 foundThings.forEach(thing => {
@@ -19,14 +27,6 @@ module.exports = {
                 message.author.send([
                     `${text}`
                 ]).catch(console.error);
-            // if it doesn't, send DM to message's author with error
-            } else {
-                message.reply({
-                    embed: {
-                      color: "RED",
-                      description: `No things starting with **${char}** exist!`
-                    }
-                }).catch(console.error);
             }
         });
     }
