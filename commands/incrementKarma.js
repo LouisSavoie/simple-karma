bba917bee102f2994126965be911988b49b0fc31
Louis Savoie <thelousavoie@gmail.com>
2021-04-26 18:41:14 -0400

    change: add cap of 9001 to karma

--- commands/incrementKarma.js
+++ commands/incrementKarma.js
@@ -9,16 +9,28 @@ module.exports = {
         if (!thingName.includes(message.member.displayName)) {
             // check if the database has the thing
             Thing.findOne({name: thingName}, function(err, foundThing) {
-                // if it does, increment thing's karma then send reply to the message's channel with thing's karma
+                // if it does, check if the thing's karma is over 9000
                 if (foundThing){
-                    foundThing.karma += 1;
-                    foundThing.save();
-                    message.reply({
-                        embed: {
-                        color: "BLUE",
-                        description: '**' + foundThing.name + '** has **' + foundThing.karma + '** karma.'
-                        }
-                    }).catch(console.error);
+                    // if it is, send reply to message's channel with error
+                    if (foundThing.karma >= 9001) {
+                        message.reply({
+                            embed: {
+                            color: "RED",
+                            description: `Thing, **${foundThing.name}'s** karma is already **OVER 9000**!\n
+                            **${foundThing.name}** doesn't need anymore.`
+                            }
+                        }).catch(console.error);
+                    // if it isn't, increment thing's karma then send reply to the message's channel with thing's karma
+                    } else {
+                        foundThing.karma += 1;
+                        foundThing.save();
+                        message.reply({
+                            embed: {
+                            color: "BLUE",
+                            description: '**' + foundThing.name + '** has **' + foundThing.karma + '** karma.'
+                            }
+                        }).catch(console.error);
+                    }
                 // if it doesn't, send reply to message's channel with error and instructions for how to create the thing
                 } else {
                     message.reply({
