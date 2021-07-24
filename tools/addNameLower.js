c5b9e80ee171c236ce9d2af9a3d839a9596b8b7a
Louis Savoie <thelousavoie@gmail.com>
2021-07-24 19:50:04 -0400

    add: add nameLower to database objects tool

---
+++ tools/addNameLower.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Thing = require('../models/thing')
+
+require('dotenv').config()
+
+// CONNECT MONGOOSE TO MONGODB
+mongoose.connect(process.env.DATABASEURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(console.log('Mongoose connected to MongoDB'))
+  .catch(error => console.log(error.message))
+mongoose.set('useFindAndModify', false)
+
+// ADD nameLower TO ALL DATABASE OBJECTS
+Thing.find({}, function (err, res) {
+  if (err) {
+    console.log(err)
+  }
+  res.forEach(thing => {
+    const nameLower = thing.name.toLowerCase();
+    thing.nameLower = nameLower;
+    thing.save();
+    console.log(thing);
+  });
+  console.log("Successed!");
+});
