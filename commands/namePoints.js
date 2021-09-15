c82cb309aae3516f60b64b921c6b1eacc5d05d74
Louis Savoie <thelousavoie@gmail.com>
2021-09-15 17:14:50 -0400

    fix: add admin permission requirement to namePoints, oops

--- commands/namePoints.js
+++ commands/namePoints.js
@@ -5,26 +5,31 @@ module.exports = {
   name: 'namePoints',
   description: 'Sets the name of the points for a server object',
   async execute (message, pointsName, debugLog, debugFlag) {
-    // check if the database already has the server
-    const [foundServer, debugDB] = await db.findServer(message.guild.id)
+    if (message.member.hasPermission('ADMINISTRATOR')) {
+      // check if the database already has the server
+      const [foundServer, debugDB] = await db.findServer(message.guild.id)
 
-    // debug
-    const debug = `  DEBUG: 2. namePoints.js, foundServer: ${foundServer}`
-    console.log(debug)
+      // debug
+      const debug = `  DEBUG: 2. namePoints.js, foundServer: ${foundServer}`
+      console.log(debug)
 
-    // if it does, change the name of the points then send reply confirming the change
-    if (foundServer) {
-      foundServer.pointsName = pointsName
-      foundServer.save()
-      reply.pointsNameSet(message, pointsName)
-      // if it doesn't, create the server then send reply to the message's channel confirming it's creation
-    } else {
-      const newThing = await db.createServer(message.guild.id, pointsName)
-      if (newThing) {
+      // if it does, change the name of the points then send reply confirming the change
+      if (foundServer) {
+        foundServer.pointsName = pointsName
+        foundServer.save()
         reply.pointsNameSet(message, pointsName)
+        // if it doesn't, create the server then send reply to the message's channel confirming it's creation
       } else {
-        reply.serverNotCreated(message)
+        const newThing = await db.createServer(message.guild.id, pointsName)
+        if (newThing) {
+          reply.pointsNameSet(message, pointsName)
+        } else {
+          reply.serverNotCreated(message)
+        }
       }
+      // if message author does not have permission, send error reply
+    } else {
+      reply.noPermission(message)
     }
 
     // if debugFlag, DM debug
