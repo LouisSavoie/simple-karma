d21704aba13c38dac483292414300ce67a81e6cb
Louis Savoie <thelousavoie@gmail.com>
2021-09-21 12:53:40 -0400

    debug info for decrementKarma undo

--- commands/decrementKarma.js
+++ commands/decrementKarma.js
@@ -20,7 +20,11 @@ module.exports = {
       foundThing.karma -= 1
       foundThing.save()
       reply.found(message, foundThing, pointsName)
-      if (addUndoFlag) undo.execute(null, message, foundThing, 'increment', null, null, pointsName)
+      if (addUndoFlag) {
+        debugLog += '\n' + debugDB + '\n' + debug
+        undo.execute(null, message, foundThing, 'increment', debugLog, debugFlag, null)
+        debugFlag = false
+      }
       // if debugFlag, DM debug
       if (debugFlag) {
         message.author.send([
